4955ced234e467e8174147bf77b4d6508120d58a
M.K <miika.kallasoja@gmail.com>
2020-04-19 18:33:36 +0300

    Added models and controllers

--- index.js
+++ index.js
@@ -4,43 +4,26 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
+const blogRouter = require('./controllers/blogs')
 require('dotenv').config()
 
-const blogSchema = mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-})
 
-const Blog = mongoose.model('Blog', blogSchema)
-
+try {
 mongoose.connect(config.mongoUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
+} catch(err) {
+    console.log(err)
+}
 
+app.use('/api/blogs', blogRouter)
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
-})
-
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
-
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
-})
 
 app.listen(config.PORT, () => {
     console.log(`Server running on port ${config.PORT}`)
 })
+
+module.exports = app
