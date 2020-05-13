8bdf760c6221d560515aa4c697651c1fcc22a994
M.K <miika.kallasoja@gmail.com>
2020-05-13 21:10:46 +0300

    User model

--- models/blog.js
+++ models/blog.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose')
 const blogSchema = mongoose.Schema({
     title: String,
     author: String,
-    url: String,
+    url: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
     likes: Number
 })
 
