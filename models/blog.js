6be6c9bfe5acc02d2475029775526af3f4710829
M.K <miika.kallasoja@gmail.com>
2020-05-16 19:56:45 +0300

    Reverting blog schema to original

--- models/blog.js
+++ models/blog.js
@@ -3,10 +3,12 @@ const mongoose = require('mongoose')
 const blogSchema = mongoose.Schema({
     title: String,
     author: String,
-    url: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-    },
+    url: String,
+
+    // url: {
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     ref: 'User'
+    // },
     likes: Number
 })
 
