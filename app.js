bb0cf8f0546e5b1ef6a92d332d482495cd88648a
M.K <miika.kallasoja@gmail.com>
2020-05-27 20:36:58 +0300

    4.18: JWT-token based login

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const config = require('./utils/config')
 const middleware = require('./utils/middleware')
 const blogRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const logger = require('./utils/logger')
 
 
@@ -27,6 +28,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
